296674243b4991cddfadd442002f5abd5e7fbf3b
YuriNikolaevitch <yuri.nikolaevitch@gmail.com>
2020-01-05 14:51:58 +0300

    Slightly updated
    At least, it works)))

---
+++ Arkanoid/Assets/Scripts/BallScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+}
---
+++ Arkanoid/Assets/Scripts/RacketScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketScript : MonoBehaviour
+{
+    public float speed = 3;
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
